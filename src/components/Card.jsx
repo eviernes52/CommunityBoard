3eb0c68f82a8f4208f22bc853f7e5ba508190114
Eviernes52 <Eviernes52@gmail.com>
2023-09-21 09:23:03 -0700

    css style changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,8 @@ export default function Card(props) {
             <img
             src={props.image} alt={props.name}
             style={{ width: "300px", height: "300px" }}/>            
-            <h1>{props.name}</h1>
-            <h2>{props.type}</h2>
+            <h3>{props.name}</h3>
+            <h4>{props.type}</h4>
             <a href={props.viewmenu} target="_blank" rel="noopener noreferrer"><button>View Menu</button></a>        </div>
     );
 }
